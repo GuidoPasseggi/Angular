38ad7780aca909ec8aae2eb6c5f57b61761540b9
GuidoPasseggi <guido.passeggi@gmail.com>
2022-08-29 21:08:24 -0300

    Hasta Clase16 (inclusive)

--- typescript/app.js
+++ typescript/app.js
@@ -1,7 +1,20 @@
-function saludar(nombre) {
-    console.table('Hola ' + nombre); // Hola Logan
-}
-var wolverine = {
-    nombre: 'Logan'
-};
-saludar(wolverine.nombre);
+"use strict";
+(function () {
+    function getEdad() {
+        return 100 + 100 + 300;
+    }
+    const nombre = "Fernando";
+    const apellido = "Herrera";
+    const edad = 33;
+    // const salida = nombre + apellido + edad;
+    // usemos un 'Template literal' para evitar
+    // tener que hacer concatenaciones con
+    // espacios y demases como en JS (ES5)
+    const salida = `
+    ${nombre}
+    ${apellido}
+    (${getEdad()})`;
+    // Fernando Herrera (Edad: 33)
+    // OJO!!! Al hace console.log podremos notar que muestra el objeto con espacios y tabulaciones incluso!!!!!!!!!!!!!!
+    console.log(salida);
+})();
---
+++ typescript/typescript/tipos_de_datos.ts
@@ -0,0 +1,21 @@
+(function(){
+
+    let mensaje: string = 'Hola';
+    let numero: number = 123;
+    let booleano: boolean = true; // false
+    let hoy: Date = new Date();
+
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+
+    let spiderman = {
+        name: 'Pedro',
+        edad: 30
+    };
+
+})();
+
+
