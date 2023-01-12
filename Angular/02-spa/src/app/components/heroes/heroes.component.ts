8de7dbd92142ef083c7d649b1dfe515ee9ec5dbe
GuidoPasseggi <guido.passeggi@gmail.com>
2023-01-11 21:06:20 -0300

    Clase 61

--- Angular/02-spa/src/app/components/heroes/heroes.component.ts
+++ Angular/02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,18 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   //es private porque solo se usa para este componente (Heroes)
-  constructor(private _heroesService: HeroesService) {
-    console.log('constructor');
+  constructor(private _heroesService: HeroesService, private router: Router) {
+    // console.log('constructor');
   }
 
   // el void no estaba en el codigo del profe, tal vez es de Angular v5
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe ', idx]);
+    // console.log(idx);
   }
 }
